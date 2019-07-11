395f4183f3b08bec937a6a16b77d660cbfc49b7a
Teste <ari.thomazini@gmail.com>
2019-07-10 21:33:55 -0300

    Alteracao do title da pagina

--- salva_categoria.php
+++ salva_categoria.php
@@ -3,7 +3,10 @@
   $nome = $_POST["nome"];
   $descricao = $_POST["descricao"];
 
+if(mysqli_query($conexao, $query)){
   echo "<p class='alert-success'> A categoria $nome - $descricao, foi cadastrada</p>";
-
+}else{
+  echo "<p class='alert-success'>Ocorreu um erro ao cadastrar categoria.</p>";
+}
 ?>
 <?php include("rodape.php"); ?>
--- salva_produto.php
+++ salva_produto.php
@@ -8,9 +8,10 @@
 
   $conexao = mysqli_connect('localhost', 'root', '', 'ecommerce');
 
-  mysqli_query($conexao, $query);
-
-  echo "<p class='alert-success'> O produto $nome, no valor $preco foi cadastrado!</p>";
-
+  if(mysqli_query($conexao, $query)){
+    echo "<p class='alert-success'> O produto $nome, no valor $preco foi cadastrado!</p>";
+  } else {
+    echo "<p class='alert-danger'> Ocorreu um problema ao cadastrar o produto.</p>";
+  }
 ?>
 <?php include("rodape.php"); ?>
